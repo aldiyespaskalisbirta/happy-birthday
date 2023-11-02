6a5e3c935a79bd4f76c84de8643ce8605c88f985
aldiyespaskalisbirta <aldiyespaskalisbirta@gmail.com>
2023-11-02 14:55:31 +0700

    happy birthday

---
+++ app/_components/photo.tsx
@@ -0,0 +1,118 @@
+import Image from "next/image";
+
+function Photo() {
+  return (
+    <div className="gallery">
+      <div className="pics">
+        <Image
+          src="/assets/images/cappie1.jpeg"
+          alt="cappie"
+          width={90}
+          height={90}
+          className="w-full h-fit"
+        />
+      </div>
+      <div className="pics">
+        <Image
+          src="/assets/images/cappie2.jpeg"
+          alt="cappie"
+          width={90}
+          height={90}
+          className="w-full h-fit"
+        />
+      </div>
+      <div className="pics">
+        <Image
+          src="/assets/images/cappie3.jpeg"
+          alt="cappie"
+          width={90}
+          height={90}
+          className="w-full h-fit"
+        />
+      </div>
+      <div className="pics">
+        <Image
+          src="/assets/images/cappie4.jpeg"
+          alt="cappie"
+          width={90}
+          height={90}
+          className="w-full h-fit"
+        />
+      </div>
+      <div className="pics">
+        <Image
+          src="/assets/images/cappie1.jpeg"
+          alt="cappie"
+          width={90}
+          height={90}
+          className="w-full h-fit"
+        />
+      </div>
+      <div className="pics">
+        <Image
+          src="/assets/images/cappie2.jpeg"
+          alt="cappie"
+          width={90}
+          height={90}
+          className="w-full h-fit"
+        />
+      </div>
+      <div className="pics">
+        <Image
+          src="/assets/images/cappie3.jpeg"
+          alt="cappie"
+          width={90}
+          height={90}
+          className="w-full h-fit"
+        />
+      </div>
+      <div className="pics">
+        <Image
+          src="/assets/images/cappie4.jpeg"
+          alt="cappie"
+          width={90}
+          height={90}
+          className="w-full h-fit"
+        />
+      </div>
+      <div className="pics">
+        <Image
+          src="/assets/images/cappie1.jpeg"
+          alt="cappie"
+          width={90}
+          height={90}
+          className="w-full h-fit"
+        />
+      </div>
+      <div className="pics">
+        <Image
+          src="/assets/images/cappie2.jpeg"
+          alt="cappie"
+          width={90}
+          height={90}
+          className="w-full h-fit"
+        />
+      </div>
+      <div className="pics">
+        <Image
+          src="/assets/images/cappie3.jpeg"
+          alt="cappie"
+          width={90}
+          height={90}
+          className="w-full h-fit"
+        />
+      </div>
+      <div className="pics">
+        <Image
+          src="/assets/images/cappie4.jpeg"
+          alt="cappie"
+          width={90}
+          height={90}
+          className="w-full h-fit"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Photo;
