5dcc30f4212c28c083f543195507226165db56e2
aldiyespaskalisbirta <aldiyespaskalisbirta@gmail.com>
2023-11-02 16:23:18 +0700

    play audio

--- app/_components/photo.tsx
+++ app/_components/photo.tsx
@@ -2,115 +2,90 @@ import Image from "next/image";
 
 function Photo() {
   return (
-    <div className="gallery">
-      <div className="pics">
-        <Image
-          src="/assets/images/cappie1.jpeg"
-          alt="cappie"
-          width={90}
-          height={90}
-          className="w-full h-fit"
-        />
-      </div>
-      <div className="pics">
-        <Image
-          src="/assets/images/cappie2.jpeg"
-          alt="cappie"
-          width={90}
-          height={90}
-          className="w-full h-fit"
-        />
-      </div>
-      <div className="pics">
-        <Image
-          src="/assets/images/cappie3.jpeg"
-          alt="cappie"
-          width={90}
-          height={90}
-          className="w-full h-fit"
-        />
-      </div>
-      <div className="pics">
-        <Image
-          src="/assets/images/cappie4.jpeg"
-          alt="cappie"
-          width={90}
-          height={90}
-          className="w-full h-fit"
-        />
-      </div>
-      <div className="pics">
-        <Image
-          src="/assets/images/cappie1.jpeg"
-          alt="cappie"
-          width={90}
-          height={90}
-          className="w-full h-fit"
-        />
-      </div>
-      <div className="pics">
-        <Image
-          src="/assets/images/cappie2.jpeg"
-          alt="cappie"
-          width={90}
-          height={90}
-          className="w-full h-fit"
-        />
-      </div>
-      <div className="pics">
-        <Image
-          src="/assets/images/cappie3.jpeg"
-          alt="cappie"
-          width={90}
-          height={90}
-          className="w-full h-fit"
-        />
-      </div>
-      <div className="pics">
-        <Image
-          src="/assets/images/cappie4.jpeg"
-          alt="cappie"
-          width={90}
-          height={90}
-          className="w-full h-fit"
-        />
-      </div>
-      <div className="pics">
-        <Image
-          src="/assets/images/cappie1.jpeg"
-          alt="cappie"
-          width={90}
-          height={90}
-          className="w-full h-fit"
-        />
-      </div>
-      <div className="pics">
-        <Image
-          src="/assets/images/cappie2.jpeg"
-          alt="cappie"
-          width={90}
-          height={90}
-          className="w-full h-fit"
-        />
-      </div>
-      <div className="pics">
-        <Image
-          src="/assets/images/cappie3.jpeg"
-          alt="cappie"
-          width={90}
-          height={90}
-          className="w-full h-fit"
-        />
-      </div>
-      <div className="pics">
-        <Image
-          src="/assets/images/cappie4.jpeg"
-          alt="cappie"
-          width={90}
-          height={90}
-          className="w-full h-fit"
-        />
-      </div>
+    <div className="flex gap-x-2 slide-in-from-left-5">
+      <Image
+        src="/assets/images/cappie1.jpeg"
+        alt="cappie"
+        width={90}
+        height={90}
+      />
+
+      <Image
+        src="/assets/images/cappie2.jpeg"
+        alt="cappie"
+        width={90}
+        height={90}
+      />
+
+      <Image
+        src="/assets/images/cappie3.jpeg"
+        alt="cappie"
+        width={90}
+        height={90}
+      />
+
+      <Image
+        src="/assets/images/cappie4.jpeg"
+        alt="cappie"
+        width={90}
+        height={90}
+      />
+
+      <Image
+        src="/assets/images/cappie1.jpeg"
+        alt="cappie"
+        width={90}
+        height={90}
+      />
+
+      <Image
+        src="/assets/images/cappie2.jpeg"
+        alt="cappie"
+        width={90}
+        height={90}
+      />
+
+      <Image
+        src="/assets/images/cappie3.jpeg"
+        alt="cappie"
+        width={90}
+        height={90}
+      />
+
+      <Image
+        src="/assets/images/cappie4.jpeg"
+        alt="cappie"
+        width={90}
+        height={90}
+      />
+
+      <Image
+        src="/assets/images/cappie1.jpeg"
+        alt="cappie"
+        width={90}
+        height={90}
+      />
+
+      <Image
+        src="/assets/images/cappie2.jpeg"
+        alt="cappie"
+        width={90}
+        height={90}
+      />
+
+      <Image
+        src="/assets/images/cappie3.jpeg"
+        alt="cappie"
+        width={90}
+        height={90}
+      />
+
+      <Image
+        src="/assets/images/cappie4.jpeg"
+        alt="cappie"
+        width={90}
+        height={90}
+      />
     </div>
   );
 }
